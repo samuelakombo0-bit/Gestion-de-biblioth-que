547109d15330d889b0efd008d94e467fa7a50f51
samuelakombo0-bit <samuelakombo0@gmail.com>
2026-09-18 14:27:14 +0200

    dernière ajout

--- script.sql
+++ script.sql
@@ -1,51 +1,36 @@
+: CREATE TABLE emprunt (
+    id_emprunt INTEGER PRIMARY KEY DEFAULT nextval('emprunt_id_emprunt_seq'),
 
-DROP TABLE IF EXISTS emprunt CASCADE;
-DROP TABLE IF EXISTS livres CASCADE;
-DROP TABLE IF EXISTS adherents CASCADE;
-DROP TABLE IF EXISTS auteurs CASCADE;
+    adherent_id INTEGER,
+    livre_id INTEGER,
 
--- TABLE AUTEURS
-CREATE TABLE auteurs (
-    id SERIAL PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL,
-    nationalite VARCHAR(100) DEFAULT 'Congo',
-    biographie TEXT
-);
-
--- TABLE ADHERENTS
-CREATE TABLE adherents (
-    id SERIAL PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL,
-    prenom VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    telephone VARCHAR(20),
-    adresse VARCHAR(255),
-    date_inscription DATE DEFAULT CURRENT_DATE
-);
-
--- TABLE LIVRES
-CREATE TABLE livres (
-    id SERIAL PRIMARY KEY,
-    titre VARCHAR(255) NOT NULL,
-    auteur_id INT REFERENCES auteurs(id) ON DELETE SET NULL,
-    categorie VARCHAR(100) DEFAULT 'Roman',
-    quantite INT NOT NULL DEFAULT 5,
-    quantite_disponible INT NOT NULL DEFAULT 5,
-    isbn VARCHAR(30),
-    annee_publication INT,
-    CONSTRAINT chk_quantite CHECK (quantite_disponible <= quantite AND quantite_disponible >= 0)
-);
-
--- TABLE EMPRUNTS
-CREATE TABLE emprunt (
-    id SERIAL PRIMARY KEY,
-    livre_id INT NOT NULL REFERENCES livres(id) ON DELETE CASCADE,
-    adherent_id INT NOT NULL REFERENCES adherents(id) ON DELETE CASCADE,
     date_emprunt DATE DEFAULT CURRENT_DATE,
-    date_retour_prevue DATE DEFAULT (CURRENT_DATE + 14),
-    date_retour DATE,
-    statut VARCHAR(20) DEFAULT 'en_cours'
+    date_retour_prevue DATE NOT NULL,
+    date_retour_reelle DATE,
+
+    CONSTRAINT emprunt_adherent_id_fkey
+        FOREIGN KEY (adherent_id)
+        REFERENCES adherents(id),
+
+    CONSTRAINT emprunt_livre_id_fkey
+        FOREIGN KEY (livre_id)
+        REFERENCES livres(id_livres)
+);CREATE TABLE adherents (
+    id INTEGER PRIMARY KEY DEFAULT nextval('adherents_id_seq'),
+    nom VARCHAR(100) NOT NULL,
+    tel VARCHAR(20) NOT NULL,
+    email VARCHAR(100) UNIQUE
 );
+ CREATE TABLE livres (
+    id_livres INTEGER PRIMARY KEY DEFAULT nextval('livres_id_livres_seq'),
+    nom_livres VARCHAR(100),
+    titre VARCHAR(100),
+    annee_publication INTEGER,
+    statut VARCHAR(30) DEFAULT 'disponible',
+    auteurs_id INTEGER,
 
-
-$$ LANGUAGE plpgsql;
+    CONSTRAINT livres_auteurs_id_fkey
+        FOREIGN KEY (auteurs_id)
+        REFERENCES auteurs(id)
+        ON DELETE CASCADE
+);
